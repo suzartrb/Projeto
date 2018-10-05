3e1061351f13abdb498cbdfab3938cd5ff51ad38
suzartrb <suzartrb@gmail.com>
2018-10-05 09:31:48 -0300

    Scanf

--- src/projeto/Projeto.java
+++ src/projeto/Projeto.java
@@ -5,7 +5,9 @@ package projeto;
 public class Projeto {
 
     public static void main(String[] args) {
+        int ler;
         System.out.println("Hi");
+        Scanner ler = new Scanner(System.in);
     }
     
 }
